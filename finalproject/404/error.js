883cb446bde6b6873899b991de98be10d97ec46c
nicatezimzade <nijatfa@code.edu.az>
2022-01-17 23:02:08 +0400

    smth

---
+++ finalproject/404/error.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    setTimeout(() => {
+        $(".preloader").fadeToggle();
+    }, 500);
+});
---
+++ finalproject/reset-password/reset-password.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    updateNav();
+    updateScroller();
+    
+    setTimeout(() => {
+        $(".preloader").fadeToggle();
+    }, 500);
+    
+    
+    $(window).scroll(function () {
+        updateNav();
+        updateScroller();
+    });
+    
+    
+    function updateNav() {
+        if ($(window).scrollTop() > 30) {
+            $(".navbar").addClass("scrolled");
+            $(".navbar-brand img").attr("src", "../home/imgs/logo.svg");
+        } else {
+            $(".navbar").removeClass("scrolled");
+            $(".navbar-brand img").attr("src", "../home/imgs/white-logo.svg");
+        }
+    }
+    
+    function updateScroller() {
+        if ($(window).scrollTop() > 0) {
+            $(".scroll-top").removeClass("d-none")
+            $(".scroll-top").fadeIn(200);
+        } else {
+            $(".scroll-top").fadeOut(200);
+        }
+    }
+    
+    $(".blog").hover(function () {
+        $(".blog-sub-menu").removeClass("d-none");
+    }, function () {
+        $(".blog-sub-menu").addClass("d-none");
+    });
+    $(".blog-sub-menu").hover(function () {
+        $(".blog-sub-menu").removeClass("d-none");
+    }, function () {
+        $(".blog-sub-menu").addClass("d-none");
+    });
+    
+    $(".pages").hover(function () {
+        $(".sub-menu").removeClass("d-none");
+    }, function () {
+        $(".sub-menu").addClass("d-none");
+    });
+    $(".sub-menu").hover(function () {
+        $(".sub-menu").removeClass("d-none");
+    }, function () {
+        $(".sub-menu").addClass("d-none");
+    });
+});
